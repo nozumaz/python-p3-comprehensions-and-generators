a54129548e743c7596df61d27f052f227c9178b6
PK <nozumaz@gmail.com>
2023-07-05 08:36:29 -0400

    return list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
